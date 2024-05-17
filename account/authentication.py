83bcd2f4ca35f105c415d56ba953c506bc821ad2
Blank <rojermen96@gmail.com>
2024-05-17 11:30:12 -0700

    Add email authentication

---
+++ account/authentication.py
@@ -0,0 +1,21 @@
+from account.repositories.Repository import UserRepository
+
+
+class EmailAuthBackend:
+    def authenticate(self, request, username=None, password=None):
+        if username is None or password is None:
+            return
+        try:
+            user = UserRepository.get(email=username)
+
+            if user.check_password(password):
+                return user
+            return None
+        except (UserRepository.model.DoesNotExist, UserRepository.model.MultipleObjectsReturned):
+            return None
+
+    def get_user(self, user_id):
+        try:
+            return UserRepository.get(pk=user_id)
+        except UserRepository.model.DoesNotExist:
+            return None
