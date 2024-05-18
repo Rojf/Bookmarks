e15c3b00143215aed5a8b079addb150e0b88a8fd
Blank <rojermen96@gmail.com>
2024-05-18 06:58:14 -0700

    Create app images

--- account/authentication.py
+++ account/authentication.py
@@ -1,4 +1,5 @@
-from account.repositories.Repository import UserRepository
+from account.repositories.Repository import (UserRepository, ProfileRepository, InfoUserRepository,
+                                             SocialMediaUserRepository)
 
 
 class EmailAuthBackend:
@@ -19,3 +20,9 @@ class EmailAuthBackend:
             return UserRepository.get(pk=user_id)
         except UserRepository.model.DoesNotExist:
             return None
+
+
+def create_profile(backend, user, *args, **kwargs):
+    ProfileRepository.model.objects.get_or_create(user=user)
+    InfoUserRepository.model.objects.get_or_create(user=user)
+    SocialMediaUserRepository.model.objects.get_or_create(user=user)
--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-yt4a=q+b(dxzfavk*)5!w6w1@pgaljizl(sfy5s8p#(w@)_gm_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_countries',
+    'social_django',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,35 @@ MIDDLEWARE = [
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'account.authentication.EmailAuthBackend',
+    'social_core.backends.facebook.FacebookOAuth2',
+    'social_core.backends.twitter.TwitterOAuth',
+    'social_core.backends.google.GoogleOAuth2',
 ]
+
+SOCIAL_AUTH_PIPELINE = [
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'account.authentication.create_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+]
+
+SOCIAL_AUTH_FACEBOOK_KEY = '425577270387346'
+SOCIAL_AUTH_FACEBOOK_SECRET = 'ea65a961b0755665974fdedba5ce776a'
+
+# SOCIAL_AUTH_TWITTER_KEY = ''
+# SOCIAL_AUTH_TWITTER_SECRET = ''
+#
+# SOCIAL_AUTH_GOOGLE_KEY = ''
+# SOCIAL_AUTH_GOOGLE_SECRET = ''
+
+SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
+
 
 ROOT_URLCONF = 'config.urls'
 
